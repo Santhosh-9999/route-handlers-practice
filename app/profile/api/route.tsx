26d532904dda39663a48f86cb9093df5d1398240
santhosh-Y <ysanthosh.ui@email.com>
2026-08-25 12:58:28 +0530

    Redirects in Route Handlers

--- app/profile/api/route.tsx
+++ app/profile/api/route.tsx
@@ -1,11 +1,23 @@
-import { headers } from "next/headers";
-export async function GET(request: Request) {
+import { headers, cookies } from "next/headers";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
   //   const requestHeaders = new Headers(request.headers);
   //   console.log("Headers", requestHeaders.get("Authorization"));
   const headerList = await headers();
   console.log("console for Auth headers", headerList.get("Authorization"));
+
+  const theme = request.cookies.get("theme");
+  console.log("cookies approch 1", theme);
+
+  const cookiesStore = await cookies();
+  console.log("cookies 2 nd approch", cookiesStore.get("theme"));
+
+  cookiesStore.set("itemsPerPage", "20");
+  console.log("cookie", cookiesStore.get("itemsPerPage"));
+
   return new Response("<h1>Profile API Data</h1>", {
-    headers: { "content-type": "text/html" },
+    headers: { "content-type": "text/html", "Set-cookie": "theme=dark" },
     status: 200,
   });
 }
